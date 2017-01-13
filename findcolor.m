a8e8c8c39128c0c1dcf24d6cd9ebc9f81af83f22
hellolzc <lzc80234@qq.com>
2017-01-13 18:29:34 +0800

    first commit

---
+++ findcolor.m
@@ -0,0 +1,84 @@
+function [] = findcolor(r,g,b)
+% this is a function
+
+NONE       =  '\033[0m'   ;
+BLACK      =  '\033[0;40m';
+L_BLACK    =  '\033[1;40m';
+RED        =  '\033[0;41m';
+L_RED      =  '\033[1;41m';
+GREEN      =  '\033[0;42m';
+L_GREEN    =  '\033[1;42m';
+BROWN      =  '\033[0;43m';
+YELLOW     =  '\033[1;43m';
+BLUE       =  '\033[0;44m';
+L_BLUE     =  '\033[1;44m';
+PURPLE     =  '\033[0;45m';
+L_PURPLE   =  '\033[1;45m';
+CYAN       =  '\033[0;46m';
+L_CYAN     =  '\033[1;46m';
+GRAY       =  '\033[0;47m';
+WHITE      =  '\033[1;47m';
+
+light0=10;
+light1=100;
+light2=200;
+
+avglight=(r+g+b)/3;
+minv=min([r,g,b]);
+diff=[r-minv;g-minv;b-minv];
+for i=1:3
+  if diff(i) > 10
+    diff(i) = 1;
+  else
+    diff(i) = 0;
+  end
+end
+
+switch diff(1)*4+diff(2)*2+diff(3)
+case 0
+  if avglight>light1
+      fprintf(WHITE);
+  else
+      fprintf(BLACK);
+  end
+case 1
+    if avglight>light1
+        fprintf(L_BLUE);
+    else
+        fprintf(BLUE);
+    end
+case 2
+    if avglight>light1
+        fprintf(L_GREEN);
+    else
+        fprintf(GREEN);
+    end
+case 3
+    if avglight>light1
+        fprintf(L_CYAN);
+    else
+        fprintf(CYAN);
+    end
+case 4
+    if avglight>light1
+        fprintf(L_RED);
+    else
+        fprintf(RED);
+    end
+case 5
+    if avglight>light1
+        fprintf(L_PURPLE);
+    else
+        fprintf(PURPLE);
+    end
+case 6
+    if avglight>light1
+        fprintf(YELLOW);
+    else
+        fprintf(BROWN);
+    end
+end
+fprintf(' ');
+fprintf(NONE);
+end
+
---
+++ printcolor.m
@@ -0,0 +1,42 @@
+% this is a script
+fprintf('Start!');
+
+
+NONE       =  '\033[0m'   ;
+BLACK      =  '\033[0;40m';
+L_BLACK    =  '\033[1;40m';
+RED        =  '\033[0;41m';
+L_RED      =  '\033[1;41m';
+GREEN      =  '\033[0;42m';
+L_GREEN    =  '\033[1;42m';
+BROWN      =  '\033[0;43m';
+YELLOW     =  '\033[1;43m';
+BLUE       =  '\033[0;44m';
+L_BLUE     =  '\033[1;44m';
+PURPLE     =  '\033[0;45m';
+L_PURPLE   =  '\033[1;45m';
+CYAN       =  '\033[0;46m';
+L_CYAN     =  '\033[1;46m';
+GRAY       =  '\033[0;47m';
+WHITE      =  '\033[1;47m';
+
+fprintf(RED);
+fprintf('Red');
+fprintf(YELLOW);
+fprintf('Yellow');
+fprintf(NONE);
+
+clear;
+inpicture=imread('colortable.jpg');
+[height,width,wei]=size(inpicture);
+fprintf('H:%d,W:%d\n',height,width);
+
+pic=imresize(inpicture,0.3);
+[height,width,wei]=size(pic);
+for i=1:height
+  for j=1:width
+    findcolor(pic(i,j,1),pic(i,j,2),pic(i,j,3));
+  end
+  fprintf('\n');
+end
+
